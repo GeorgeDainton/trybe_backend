aa152f551ee1cb68849b67c03f7e096c4ce24022
George Dainton <gcdainton94@gmail.com>
2022-07-06 13:58:24 +0100

    Goal model set up

--- trybe_api/models.py
+++ trybe_api/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Goal(models.Model):
+    goal_description = models.CharField(max_length=180)
+
+    def __str__(self):
+        return self.goal_description
