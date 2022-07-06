3f7f7dab7fec9a80da5c3588deb23ce113f76222
LGretzk <LGretzk@protonmail.com>
2022-07-06 16:08:31 +0100

    Rest framework get request working

--- trybe_api/serializers.py
+++ trybe_api/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 
 from .models import Goal
 
-class GoalSerializer(serializers.HyperlinkedModelSerializzer):
+class GoalSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Goal
         fields = ('id', 'goal_description')
