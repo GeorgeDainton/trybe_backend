d4cbfe9250fa7e4623088d933227e63453019781
K-Carty <kieran.carty@me.com>
2022-07-06 15:17:22 +0100

    Failed rest framework set-up

---
+++ trybe_api/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers 
+from .models import Goal
+
+class GoalSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Goal
+        fields = ('id', 'goal')
+
+
--- trybe_api/views.py
+++ trybe_api/views.py
@@ -1,3 +1,16 @@
+import imp
+from django.http import QueryDict
 from django.shortcuts import render
 
 # Create your views here.
+
+from rest_framework import viewsets 
+
+from .serializers import GoalSerializer
+from .models import Goal
+
+class GoalViewSet(viewsets.ModelViewSet):
+    queryset = Goal.objects.all()
+    serializer_class = GoalSerializer
+
+   
