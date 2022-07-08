6b667ac608ca6f733ede35b1882553ca3c96f2dd
K-Carty <kieran.carty@me.com>
2022-07-08 12:34:24 +0100

    manual connection to postgres

--- trybe_backend/settings.py
+++ trybe_backend/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = 'trybe_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dbvet16nbo3ons',
+        'USER': 'qmldvhthvquowt',
+        'PASSWORD':'ade4addaa25719c8da40b95a3ebae70d428e4515caf5cfdf3b1081c80972f354',
+        'HOST': 'ec2-34-239-241-121.compute-1.amazonaws.com',
+        'PORT': '5432',
     }
 }
 
