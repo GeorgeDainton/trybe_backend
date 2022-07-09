3041ef620794258ee1b7f1a7c247772269494377
LGretzk <LGretzk@protonmail.com>
2022-07-09 18:45:37 +0100

    Add test

--- trybe_api/test/test_goal_api.py
+++ trybe_api/test/test_goal_api.py
@@ -1,38 +1,36 @@
-from django.test import TestCase
-from django.urls import reverse
-
-from rest_framework.test import APIClient
+from rest_framework.test import APITestCase
 from rest_framework import status
-
-from trybe_api.serializers import GoalSerializer
 from trybe_api.models import Goal
 
-# get_goals_url = reverse('goals')
-# create_url = reverse('goals')
-
-# def sample_payload():
-#     payload = {
-#       "goal_description" : "Do homework"
-#     }
-#     return Goal.objects.create(**payload)
-
-
-class GoalApiTest(TestCase):
-    def setUp(self):
-        self.client = APIClient()
-
+class GoalApiTest(APITestCase):
     def test_get_goal_list(self):
+        payload = {'goal_description': 'Do something else'}
+        self.client.post('/goals/', payload, format='json')
         res = self.client.get('/goals/')
 
         self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(Goal.objects.count(), 1)
 
     def test_create_goal(self):
-        payload = {
-          "goal_descrption": "Do homework"
-        }
-
-        res = self.client.post('/goals/', payload)
-        print(res)
+        payload = {'goal_description': 'Do homework'}
+        res = self.client.post('/goals/', payload, format='json')
 
         self.assertEqual(res.status_code, status.HTTP_201_CREATED)
-        # self.assertEqual(res.data, {'id': {'\d'}, 'goal_description': 'Do homework'})
+        self.assertEqual(Goal.objects.count(), 1)
+        self.assertEqual(Goal.objects.get().goal_description, 'Do homework')
+
+    def test_delete_goal(self):
+        payload = {'goal_description': 'Do something else'}
+        self.client.post('/goals/', payload, format='json')
+        res = self.client.delete('/goals/1/')
+
+        self.assertEqual(res.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(Goal.objects.count(), 0)
+
+    def test_update_goal(self):
+        payload = {'goal_description': 'Do something else'}
+        payload2 = {'goal_description': 'Have a rest'}
+        self.client.post('/goals/', payload, format='json')
+        res = self.client.put('/goals/1/', payload2, format='json')
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
