4d4f88dd9d779f546401bcdab9a679903b7b6d77
K-Carty <kieran.carty@me.com>
2022-07-11 11:04:42 +0100

    settings test3

--- trybe_backend/settings.py
+++ trybe_backend/settings.py
@@ -105,32 +105,18 @@ WSGI_APPLICATION = 'trybe_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databasesheroku
 
-ON_HEROKU = False
 
-if ON_HEROKU:
-
-    DATABASES = {
-        'default': {
-            'ENGINE':'django.db.backends.postgresql',
-            'NAME': os.getenv('NAME'),
-            'USER': os.getenv('USER'),
-            'PASSWORD': os.getenv('PASSWORD'),
-            'HOST': os.getenv('HOST'),
-            'PORT': os.getenv('PORT'),
-        }
+DATABASES = {
+    'default': {
+        'ENGINE':'django.db.backends.postgresql',
+        'NAME': os.getenv('NAME'),
+        'USER': os.getenv('USER'),
+        'PASSWORD': os.getenv('PASSWORD'),
+        'HOST': os.getenv('HOST'),
+        'PORT': os.getenv('PORT'),
     }
+}
   
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE':'django.db.backends.postgresql',
-            'NAME': os.getenv('LOCAL_NAME'),
-            'USER': os.getenv('LOCAL_USER'),
-            'PASSWORD': os.getenv('LOCAL_PASSWORD'),
-            'HOST': os.getenv('LOCAL_HOST'),
-            'PORT': os.getenv('LOCAL_PORT'),
-        }
-  }
 
 
 # Password validation
